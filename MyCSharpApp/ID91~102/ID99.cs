8debeff750b8052180dc525055809dca1e1beb27
jiwoong <165646929+ziwung@users.noreply.github.com>
2026-08-15 14:26:44 +0000

    ID99~100+

--- MyCSharpApp/ID91~102/ID99.cs
+++ MyCSharpApp/ID91~102/ID99.cs
@@ -1,15 +1,72 @@
-using System;
-using System.Linq;
-using System.Collections.Generic;
-using System.Runtime.Intrinsics.X86;
-class ID99{
-    static void Main(){
-        int N = int.Parse(Console.ReadLine()!);
-        int[,] nods = new int[N,2];
-        for(int i = 0; i<N; i++){
-            int[] input = Console.ReadLine()!.Split(' ').Select(int.Parse).ToArray();
-            nods[i,0]= input[0];
-            nods[i,1]= input[1];
-        }
-    }
-}
+// using System;
+// using System.Collections.Generic;
+// using System.IO;
+// using System.Threading;
+
+// class ID99
+// {
+//     static void Main()
+//     {
+//         // 깊은 재귀 호출을 위해 스택 크기를 64MB로 늘린 새로운 스레드에서 실행
+//         Thread t = new Thread(Solve, 1024 * 1024 * 64);
+//         t.Start();
+//         t.Join();
+//     }
+
+//     static void Solve()
+//     {
+//         using (StreamReader sr = new StreamReader(Console.OpenStandardInput()))
+//         using (StreamWriter sw = new StreamWriter(Console.OpenStandardOutput()))
+//         {
+//             string line = sr.ReadLine();
+//             if (string.IsNullOrEmpty(line)) return;
+//             int N = int.Parse(line);
+
+//             // 인접 리스트 생성
+//             List<int>[] G = new List<int>[N + 1];
+//             for (int i = 0; i <= N; i++)
+//             {
+//                 G[i] = new List<int>();
+//             }
+
+//             for (int i = 0; i < N - 1; i++)
+//             {
+//                 string[] parts = sr.ReadLine().Split();
+//                 int u = int.Parse(parts[0]);
+//                 int v = int.Parse(parts[1]);
+//                 G[u].Add(v);
+//                 G[v].Add(u);
+//             }
+
+//             bool[] visited = new bool[N + 1];
+//             int[] dp = new int[N + 1];
+
+//             // DFS 실행
+//             Dfs(1, G, visited, dp);
+
+//             // 답을 계산 (오버플로우 방지를 위해 long 타입 사용)
+//             long answer = 0;
+//             for (int i = 2; i <= N; i++)
+//             {
+//                 answer += (long)dp[i] * (N - dp[i]);
+//             }
+
+//             sw.WriteLine(answer);
+//         }
+//     }
+
+//     // 깊이 우선 탐색 함수
+//     static void Dfs(int pos, List<int>[] G, bool[] visited, int[] dp)
+//     {
+//         visited[pos] = true;
+//         dp[pos] = 1;
+//         foreach (int i in G[pos])
+//         {
+//             if (!visited[i])
+//             {
+//                 Dfs(i, G, visited, dp);
+//                 dp[pos] += dp[i];
+//             }
+//         }
+//     }
+// }
--- MyCSharpApp/obj/Debug/net10.0/MyCSharpApp.AssemblyInfo.cs
+++ MyCSharpApp/obj/Debug/net10.0/MyCSharpApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyCSharpApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6d0d2e9824834843c3fca219cce44a8e5c2bee0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5007121bc7420e1e0ce1904c13cfe475d1a58e10")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyCSharpApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyCSharpApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
