5baacdd1c6c13b395d7ee649bf00692b178acc4f
jiwoong <165646929+ziwung@users.noreply.github.com>
2026-09-08 14:05:15 +0000

    Q28,29 +

--- MyCSharpApp/QuestionAlgorithm/Q26~30.cs
+++ MyCSharpApp/QuestionAlgorithm/Q26~30.cs
@@ -2,6 +2,120 @@ using System;
 using System.IO;
 using System.Linq;
 using System.Collections.Generic;
+class Q30
+{
+    private static StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+    private static StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+    private static  string[] line = new string[0];
+    private static int lineIdx = 0;
+
+    private static string NextToken(){
+        while(lineIdx >= line.Length){
+            string s = sr.ReadLine()!;
+            if(s == null) return null;
+            line = s.Split(' ',StringSplitOptions.RemoveEmptyEntries);
+            lineIdx = 0;
+        }
+        return line[lineIdx++];
+    }
+    private static int NextInt() => int.Parse(NextToken())!;
+    private static char NextChar() => char.Parse(NextToken());
+    static void Main()
+    {
+        
+    }
+}
+// class Q29
+// {
+//     private static StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+//     private static StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+//     private static  string[] line = new string[0];
+//     private static int lineIdx = 0;
+
+//     private static string NextToken(){
+//         while(lineIdx >= line.Length){
+//             string s = sr.ReadLine()!;
+//             if(s == null) return null;
+//             line = s.Split(' ',StringSplitOptions.RemoveEmptyEntries);
+//             lineIdx = 0;
+//         }
+//         return line[lineIdx++];
+//     }
+//     private static int NextInt() => int.Parse(NextToken())!;
+//     private static char NextChar() => char.Parse(NextToken());
+//     static void Main()
+//     {
+//         // A29 & B29
+//         long a = NextInt();
+//         long b = NextInt();
+//         int i=0; long ans = 1; int tm = 1000000007;
+//         while (b >= 1L<<i)
+//         {
+//             if((b&1L<<i++) > 0)
+//             {
+//                 ans *= a;
+//                 ans %= tm;
+//             }
+//             a = (a*a) % tm;
+//         }
+//         sw.WriteLine(ans);
+//         sw.Close();
+//     }
+// }
+// class Q28
+// {
+//     private static StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+//     private static StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+//     private static  string[] line = new string[0];
+//     private static int lineIdx = 0;
+
+//     private static string NextToken(){
+//         while(lineIdx >= line.Length){
+//             string s = sr.ReadLine()!;
+//             if(s == null) return null;
+//             line = s.Split(' ',StringSplitOptions.RemoveEmptyEntries);
+//             lineIdx = 0;
+//         }
+//         return line[lineIdx++];
+//     }
+//     private static int NextInt() => int.Parse(NextToken())!;
+//     private static char NextChar() => char.Parse(NextToken());
+//     static void Main()
+//     {
+//         // // A28
+//         // int N = NextInt();
+//         // char[] sign = new char[N];
+//         // int[] num = new int[N];
+//         // for(int i =0; i<N; i++)
+//         // {
+//         //     sign[i] = NextChar();
+//         //     num[i] = NextInt(); 
+//         // }
+//         // int ans = 0;
+//         // for(int i = 0 ;i<N; i++)
+//         // {
+//         //     if(sign[i] == '+') ans += num[i];
+//         //     else if(sign[i] == '-') ans -= num[i];
+//         //     else ans *= num[i];
+
+//         //     if(ans<0) ans += 10000;
+//         //     ans%=10000; 
+//         // }
+//         // sw.WriteLine(ans);
+//         // sw.Close();
+
+//         // // B28 (기본적인 풀이)
+//         // int N = NextInt();
+//         // int thenum = 1000000007;
+//         // int[] pibonachi = new int[N+1]; pibonachi[1] = 1; pibonachi[2] = 1;
+//         // for(int i = 3; i <= N; i++)
+//         // {
+//         //     pibonachi[i] = (pibonachi[i-1] + pibonachi[i-2])%thenum;
+//         // }
+//         // sw.WriteLine(pibonachi[N]);
+//         // sw.Close();
+//     }
+// }
 // class Q27
 // {
 //     private static StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
--- MyCSharpApp/obj/Debug/net10.0/MyCSharpApp.AssemblyInfo.cs
+++ MyCSharpApp/obj/Debug/net10.0/MyCSharpApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyCSharpApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4719efc4308d6d5db697407fe61cfc42eabf7a2e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f503e72ad40ee4c407d761598439664b02d89748")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyCSharpApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyCSharpApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
