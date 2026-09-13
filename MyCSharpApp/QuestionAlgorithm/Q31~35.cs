751fface7e0070dc68e9b2c38f1fd128b63c88b3
jiwoong <165646929+ziwung@users.noreply.github.com>
2026-09-13 13:26:58 +0000

    Q32

--- MyCSharpApp/QuestionAlgorithm/Q31~35.cs
+++ MyCSharpApp/QuestionAlgorithm/Q31~35.cs
@@ -2,35 +2,121 @@ using System;
 using System.IO;
 using System.Linq;
 using System.Collections.Generic;
-class Q32
-{
-    private static StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
-    private static StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
-    private static  string[] line = new string[0];
-    private static int lineIdx = 0;
-
-    private static string NextToken(){
-        while(lineIdx >= line.Length){
-            string s = sr.ReadLine()!;
-            if(s == null) return null!;
-            line = s.Split(' ',StringSplitOptions.RemoveEmptyEntries);
-            lineIdx = 0;
-        }
-        return line[lineIdx++];
-    }
-    private static int NextInt() => int.Parse(NextToken())!;
-    private static char NextChar() => char.Parse(NextToken());
-    static void Main()
-    {
-        // A32
-        int n = NextInt(); int a = NextInt(); int b = NextInt();
-        (int max, int min) = (a>b) ? (a,b) : (b,a);
-        if (n - max)
-        {
-            
-        }
-    }
+class Q34{
+    
 }
+// class Q33
+// {
+//     private static StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+//     private static StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+//     private static  string[] line = new string[0];
+//     private static int lineIdx = 0;
+
+//     private static string NextToken(){
+//         while(lineIdx >= line.Length){
+//             string s = sr.ReadLine()!;
+//             if(s == null) return null!;
+//             line = s.Split(' ',StringSplitOptions.RemoveEmptyEntries);
+//             lineIdx = 0;
+//         }
+//         return line[lineIdx++];
+//     }
+//     private static int NextInt() => int.Parse(NextToken())!;
+//     static void Main()
+//     {
+//         // // A33
+//         // int n = NextInt();
+//         // int[] a = new int[n];
+//         // for(int i = 0; i < n; i++) a[i] = NextInt();
+//         // int XORsum = a[0];
+//         // for(int i = 1; i < n; i++)
+//         // {
+//         //     XORsum = XORsum^a[i];
+//         //     Console.WriteLine(XORsum);
+//         // }
+//         // if(XORsum == 0)
+//         // {
+//         //     Console.WriteLine("후수필승");
+//         // }else Console.WriteLine("선수필승");
+
+//         // // B33
+//         // int h = NextInt();
+//         // int w = NextInt();
+//         // if((h^w) == 0){
+//         //     Console.WriteLine("후수필승");
+//         // }else Console.WriteLine("선두필승");
+//     }
+// }
+// class Q32
+// {
+//     private static StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+//     private static StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+//     private static  string[] line = new string[0];
+//     private static int lineIdx = 0;
+
+//     private static string NextToken(){
+//         while(lineIdx >= line.Length){
+//             string s = sr.ReadLine()!;
+//             if(s == null) return null!;
+//             line = s.Split(' ',StringSplitOptions.RemoveEmptyEntries);
+//             lineIdx = 0;
+//         }
+//         return line[lineIdx++];
+//     }
+//     private static int NextInt() => int.Parse(NextToken())!;
+//     static void Main()
+//     {
+//         // // A32
+//         // int n = NextInt(); int a = NextInt(); int b = NextInt();
+//         // (int max, int min) = (a>b) ? (a,b):(b,a);
+//         // int[] dp = new int[n+1+max]; 
+//         // for(int i = n+max; i >= n-min; i--)
+//         // {
+//         //     dp[i] = -1;
+//         // }
+//         // for(int i = n-min-1; i >= 0; i--)
+//         // {
+//         //     if(dp[i+a] == -1 || dp[i+b] == -1)
+//         //     {
+//         //         dp[i] = 1;
+//         //     }
+//         //     else
+//         //     {
+//         //         dp[i] = -1;
+//         //     }
+//         // }
+//         // Console.WriteLine(dp[0]);
+
+//         // B32
+//         int n = NextInt();
+//         int k = NextInt();
+//         int[] a = new int[k];
+//         for(int i = 0; i < k; i++)
+//         {
+//             a[i] = NextInt();
+//         }
+//         int max = a.Max(); int min = a.Min();
+//         int[] dp = new int[n+max+1];
+//         for(int i = n; i<n+max; i++)
+//         {
+//             dp[i] = -1;
+//         }
+//         for(int i = n-1; i >= 0; i--)
+//         {
+//             bool canWin = false;
+//             foreach(int x in a)
+//             {
+//                 if(dp[i + x] == -1)
+//                 {
+//                     canWin = true;
+//                     break;
+//                 }
+//             }
+//             dp[i] = canWin ? 1 : -1;
+//         }
+//         Console.WriteLine(dp[0]);
+//     }
+// }
 // class Q31
 // {
 //     private static StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
--- MyCSharpApp/obj/Debug/net10.0/MyCSharpApp.AssemblyInfo.cs
+++ MyCSharpApp/obj/Debug/net10.0/MyCSharpApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyCSharpApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20bfb3a658638d0a936488c8b4e6474c74d99814")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36fbded628b799f58869cf1d9f345e5cf7666d3f")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyCSharpApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyCSharpApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
