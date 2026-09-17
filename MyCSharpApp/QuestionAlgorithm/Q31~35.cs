540abadb6284e34412bd2f2f34859491a5abf95e
jiwoong <165646929+ziwung@users.noreply.github.com>
2026-09-17 14:22:19 +0000

    Q33~35 +

--- MyCSharpApp/QuestionAlgorithm/Q31~35.cs
+++ MyCSharpApp/QuestionAlgorithm/Q31~35.cs
@@ -1,10 +1,132 @@
-using System;
-using System.IO;
-using System.Linq;
-using System.Collections.Generic;
-class Q34{
-    
-}
+// using System;
+// using System.IO;
+// using System.Linq;
+// using System.Collections.Generic;
+// class Q35
+// {
+//     private static StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+//     private static StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+//     private static  string[] line = new string[0];
+//     private static int lineIdx = 0;
+//     private static string NextToken(){
+//         while(lineIdx >= line.Length){
+//             string s = sr.ReadLine()!;
+//             if(s == null) return null!;
+//             line = s.Split(' ',StringSplitOptions.RemoveEmptyEntries);
+//             lineIdx = 0;
+//         }
+//         return line[lineIdx++];
+//     }
+//     private static int NextInt() => int.Parse(NextToken())!;
+//     static void Main()
+//     {
+//         // A35
+//         int n = NextInt();
+//         int[,] dp = new int[n+1,n+1];
+//         for(int i = 1; i <= n; i++)
+//         {
+//             dp[n,i] = NextInt();
+//         }
+//         for(int i = n-1; i > 0; i--)
+//         {
+//                 for(int j = i; j >0; j--){
+//                 if(i%2 == 1) // 최대인경우 턴
+//                 {
+//                     dp[i,j] = Math.Max(dp[i+1,j], dp[i+1,j+1]);
+//                 }
+//                 else //최소 턴
+//                 {
+//                     dp[i,j] = Math.Min(dp[i+1,j], dp[i+1,j+1]);
+//                 }
+//             }
+//         }
+//         Console.WriteLine(dp[1,1]);
+//     }
+// }
+// class Q34{
+//     private static StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+//     private static StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+//     private static  string[] line = new string[0];
+//     private static int lineIdx = 0;
+//     private static string NextToken(){
+//         while(lineIdx >= line.Length){
+//             string s = sr.ReadLine()!;
+//             if(s == null) return null!;
+//             line = s.Split(' ',StringSplitOptions.RemoveEmptyEntries);
+//             lineIdx = 0;
+//         }
+//         return line[lineIdx++];
+//     }
+//     private static int NextInt() => int.Parse(NextToken())!;
+//     static void Main()
+//     {
+//         // // A34
+//         // int n = NextInt(); int x = NextInt(); int y = NextInt();
+//         // int[] a = new int[n];
+//         // for(int i = 0; i < n; i++)
+//         // {
+//         //     a[i] = NextInt(); // 커스텀 메서드 입력
+//         // }
+//         // int xorSum = GetGrundy(a[0],x,y);
+//         // for(int i = 1; i<n; i++)
+//         // {
+//         //     xorSum = xorSum^GetGrundy(a[i],x,y);
+//         // }
+//         // if(xorSum == 0)
+//         // {
+//         //     Console.WriteLine("후수필승");
+//         // }else Console.WriteLine("선수필승");
+
+//         // B34
+//         int n = NextInt();
+//         int[] a = new int[n];
+//         for(int i = 0; i < n; i++)
+//         {
+//             a[i] = NextInt(); // 커스텀 메서드 입력
+//         }
+//         int xorSum = GetGrundy2(a[0]);
+//         for(int i = 1; i<n; i++)
+//         {
+//             xorSum = xorSum^GetGrundy2(a[i]);
+//         }
+//         if(xorSum == 0)
+//         {
+//             Console.WriteLine("후수필승");
+//         }else Console.WriteLine("선수필승");
+//     }
+//     public static int GetGrundy2(int a)
+//     {
+//         int ans = a%5;
+//         if (ans == 4)
+//         {
+//             return 2;
+//         }else if(ans < 4 && ans > 1)
+//         {
+//             return 1;
+//         }else return 0;
+//     }
+//     public static int GetGrundy(int a, int x, int y)
+//     {
+//         int[] num = new int[a+1];
+//         (int min, int max) = (x>y) ? (y,x) : (x,y);
+//         for(int i =0; i<min; i++)
+//         {
+//             num[i] = 0;
+//         }
+//         for(int i = min; i<a+1; i++)
+//         {
+//             int[] nums = new int[3];
+//             for(int j = 0; j<3; j++)
+//             {
+//                 nums[j] = j;
+//             }
+//             nums[num[i-min]] = int.MaxValue;
+//             if(i>=max) nums[num[i-max]] = int.MaxValue;
+//             num[i] = nums.Min();
+//         }
+//         return num[a];
+//     }
+// }
 // class Q33
 // {
 //     private static StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
